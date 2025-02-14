080eea515012cbac2e6ab9f138cb598701b3a3bb
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-02-15 03:32:30 +0530

    Day 8 Work

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+const addEventOnElements = function(elements,eventType,callback){
+  for(let i=0,len=elements.length;i<len;i++){
+    elements[i].addEventListener(eventType,callback);
+  }
+}
