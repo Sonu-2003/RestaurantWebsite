4bf00843484e09db61c88913b70f93b005dee35c
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-02-21 04:33:53 +0530

    Day13

--- assets/script.js
+++ assets/script.js
@@ -59,3 +59,38 @@ addEventOnElements([heroSliderNextBtn,heroSliderPrevBtn], "mouseover",function()
 });
 addEventOnElements([heroSliderNextBtn,heroSliderPrevBtn], "mouseout",autoSlide);
 window.addEventListener("load",autoSlide);
+
+const navbar=document.querySelector("[data-navbar]");
+const navTogglers=document.querySelectorAll("[data-nav-toggler]");
+const overlay=document.querySelector("[data-overlay]");
+
+const toggleNavbar=function(){
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers,"click",toggleNavbar);
+
+const header=document.querySelector("[data-header]");
+const backTopBtn=document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos=0;
+const hiderHeader=function(){
+  const isScrollBottom =lastScrollPos<window.scrollY;
+  if(isScrollBottom){
+    header.classList.add("hide");
+  } else{
+    header.classList.remove("hide");
+  }
+  lastScrollPos=window.scrollY;
+}
+window.addEventListener("scroll",function(){
+  if(this.window.scrollY>=50{
+    header.classList.add("active");
+    hiderHeader();
+  } else{
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
