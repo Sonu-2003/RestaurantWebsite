164aedba68eee352b5844c2e08ad6cce36fe6e48
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-03-06 04:37:58 +0530

    06/03/2025

--- assets/script.js
+++ assets/script.js
@@ -94,3 +94,19 @@ window.addEventListener("scroll",function(){
     backTopBtn.classList.remove("active");
   }
 });
+
+const parallexItems=parallexItems=document.querySelectorAll("[data-parallax-item]");
+let x,y;
+window.addEventListener("mousemove",function(event){
+  x=(event.clientX/window.innerWidth*10)-5;
+  y=(event.clientY/window.innerHeight*10)-5;
+
+x=x-(x*2);
+y=y-(y*2);
+for(let i=0,len=parallexItems.length;i<len;i++){
+  x=x*Number(parallexItems[i].dataset.parallexSpeed);
+  y=y*Number(parallexItems[i].dataset.parallexSpeed);
+  parallexItems[i].style.transform=`translate3d(${x}px,${y}px,0px)`;
+}
+});
+
