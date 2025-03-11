74bed4fc1694e3493c4beff9d882d50dc3beb3eb
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-03-12 03:24:57 +0530

    12/03/2025

--- assets/script.js
+++ assets/script.js
@@ -95,18 +95,18 @@ window.addEventListener("scroll",function(){
   }
 });
 
-const parallexItems=parallexItems=document.querySelectorAll("[data-parallax-item]");
+const parallexItems=document.querySelectorAll("[data-parallax-item]");
 let x,y;
 window.addEventListener("mousemove",function(event){
   x=(event.clientX/window.innerWidth*10)-5;
   y=(event.clientY/window.innerHeight*10)-5;
 
-x=x-(x*2);
-y=y-(y*2);
-for(let i=0,len=parallexItems.length;i<len;i++){
-  x=x*Number(parallexItems[i].dataset.parallexSpeed);
-  y=y*Number(parallexItems[i].dataset.parallexSpeed);
-  parallexItems[i].style.transform=`translate3d(${x}px,${y}px,0px)`;
+  x=x-(x*2);
+  y=y-(y*2);
+  for(let i=0,len=parallexItems.length;i<len;i++){
+    x=x*Number(parallexItems[i].dataset.parallexSpeed);
+    y=y*Number(parallexItems[i].dataset.parallexSpeed);
+    parallexItems[i].style.transform=`translate3d(${x}px,${y}px,0px)`;
 }
 });
 
